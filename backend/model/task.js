232d625d35d2e8d7fa434aaba487315a3ebce38b
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-10 14:25:45 -0700

    added task.js model

---
+++ backend/model/task.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const List = require('./list.js');
+
+const taskSchema = Schema({
+  content: {type: String, required: true},
+  list: {type: Schema.Types.ObjectId, required: true, ref: 'list'}
+});
+
+module.exports = mongoose.model('task', taskSchema);
