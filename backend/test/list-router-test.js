23e6671abd2963b68dfc9dd0a9eb096421af7109
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-10 15:47:30 -0700

    add clearDB() function to afterEach hook

--- backend/test/list-router-test.js
+++ backend/test/list-router-test.js
@@ -7,14 +7,16 @@ const superagent = require('superagent');
 require('dotenv').config();
 
 const server = require('../lib/server.js');
+const clearDB = require('../lib/clear-db.js');
 
 const API_URL = process.env.API_URL;
 let tempList;
 
 
 describe('testing /api/lists', () => {
-  before(server.start)
-  after(server.stop)
+  before(server.start);
+  after(server.stop);
+  afterEach(clearDb);
 
   describe('testing POST /api/lists', () => {
     let data = {title: faker.name.title()}
