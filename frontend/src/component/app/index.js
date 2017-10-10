ff5f82c712ae133bd2939c6b39f4e7fa6bc2b5e7
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-09 18:33:27 -0700

    imported createAppStore

--- frontend/src/component/app/index.js
+++ frontend/src/component/app/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
+import Provider from 'react-redux';
+import createAppStore from '../../lib/create-app-store';
 import {BrowserRouter, Route} from 'react-router-dom';
+
+const store = createAppStore();
 
 class App extends React.Component {
   render() {
     return(
-      <div className='app'>
-        <BrowserRouter>
-          <main>
-            <h1>cool stuff</h1>
-          </main>
-        </BrowserRouter>
-      </div>
+      <Provider store={store}>
+        <div className='app'>
+          <BrowserRouter>
+            <main>
+              <h1>cool stuff</h1>
+            </main>
+          </BrowserRouter>
+        </div>
+      </Provider>
     );
   }
 }
