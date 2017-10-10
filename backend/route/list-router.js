fc740efe0426ab8cec300d1a2b96ade58ba2e4cb
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-10 14:52:35 -0700

    added POST route in list-router.js

--- backend/route/list-router.js
+++ backend/route/list-router.js
@@ -1,4 +1,14 @@
 'use strict';
 
-
+const jsonParser = require('body-parser').json();
 const listRouter = module.exports = new require('express').Router();
+
+const List = require('../model/list.js');
+
+listRouter.post('/api/lists', jsonParser, (req, res, next) => {
+  console.log('hit /api/lists');
+
+  new List(req.body).save()
+  .then(list => res.json(list))
+  .catch(next)
+})
