b20b71229f0fb5998d9a30a469afd00a61de3c87
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-11 11:31:08 -0700

    added second test for list-router-test, fixed typos

--- backend/test/list-router-test.js
+++ backend/test/list-router-test.js
@@ -7,17 +7,16 @@ const superagent = require('superagent');
 require('dotenv').config();
 
 const server = require('../lib/server.js');
-const clearDB = require('../lib/clear-db.js');
-const mockList = require('../lib/mock-list.js');
+const clearDB = require('./lib/clear-db.js');
+const mockList = require('./lib/mock-list.js');
 
-let tempList;
 const API_URL = process.env.API_URL;
 
 
 describe('testing /api/lists', () => {
   before(server.start);
+  afterEach(clearDB);
   after(server.stop);
-  afterEach(clearDb);
 
   describe('testing POST /api/lists', () => {
     let data = {title: faker.name.title()}
@@ -28,19 +27,25 @@ describe('testing /api/lists', () => {
         expect(res.status).toEqual(200);
         expect(res.body.title).toEqual(data.title);
         expect(res.body.tasks).toEqual([]);
-        // expect(res.body._id).toExist()
+        expect(res.body).toHaveProperty('_id');
         console.log(res.body);
       })
     })
   })
 
   describe('testing GET /api/lists', () => {
-
+    let tempList;
     it('should return a list', () => {
       mockList()
-      .then(list => superagent.get(`${API_URL}/api/lists/${list._id}`))
+      .then(list => {
+        tempList = list;
+        return superagent.get(`${API_URL}/api/lists/${list._id}`)
+       })
       .then( res => {
-        expect(true).toBe(true);
+        console.log('res.body', res.body)
+        // expect(res.status).toEqual(200);
+        // expect(res.body.title).toEqual(tempList.title);
+        // expect(res.body.tasks).toEqual([]);
       })
     })
   })
