3c862d1f2394b90b33445c7c873481f7409a2259
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-13 14:42:14 -0700

    small naming conv change for mockList.createMany()

--- backend/test/lib/mock-list.js
+++ backend/test/lib/mock-list.js
@@ -14,8 +14,8 @@ mockList.createOne = () => {
 
 mockList.createMany = (n) => {
   //filling with zeroes to use map
-  let mockListArray = new Array(n)
+  let mockListPromises = new Array(n)
     .fill(0).map(() => mockList.createOne());
 
-  return Promise.all(mockListArray)
+  return Promise.all(mockListPromises)
 }
