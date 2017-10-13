2ad8e55236e3601ea857cfddc003f806a4392fbf
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-13 14:33:49 -0700

    add mockTask.createOne() for testing

--- backend/test/lib/mock-task.js
+++ backend/test/lib/mock-task.js
@@ -1,10 +1,28 @@
 'use strict';
 
+const faker = require('faker');
 const mockList = require('./mock-list.js');
 const Task = require('../../model/task.js');
 
 const mockTask = module.exports = {};
 
 mockTask.createOne = () => {
+  let result = {};
+  mockList.createOne()
+  .then( list => {
+    result.list = list;
+    new Task({
+      content: faker.random.words(3),
+      listID: list._id.toString()
+    })
+    .save()
+  })
+  .then(task => {
+    result.task = task;
+    return result;
+  })
+}
+
+mockTask.createMany = (n) => {
 
 }
