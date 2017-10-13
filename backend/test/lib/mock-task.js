da1bccb0ad2bd332f6718c5931344049d2b7ae99
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-13 14:29:52 -0700

    add mock-task.js

---
+++ backend/test/lib/mock-task.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const mockList = require('./mock-list.js');
+const Task = require('../../model/task.js');
+
+const mockTask = module.exports = {};
+
+mockTask.createOne = () => {
+
+}
