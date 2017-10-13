29a3999c6174feb4c872537444301c2845001494
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-13 13:25:36 -0700

    add first pre hook to taskSchema

--- backend/model/task.js
+++ backend/model/task.js
@@ -7,9 +7,13 @@ const List = require('./list.js');
 
 const taskSchema = Schema({
   content: {type: String, required: true},
-  list: {type: Schema.Types.ObjectId, required: true, ref: 'list'}
+  listID: {type: Schema.Types.ObjectId, required: true, ref: 'list'}
 });
 
-
+taskSchema.pre('save', function(next) {
+  List.findById(this.listID)
+  .then( () => next())
+  .catch( () => next(new Error('failed to create note - list does not exist')))
+})
 
 module.exports = mongoose.model('task', taskSchema);
