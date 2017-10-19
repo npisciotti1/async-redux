2bca588a619b4052cee30bd808fb26f54c912851
Nikko Pisciotti <nikkopisciotti@gmail.com>
2017-10-19 16:52:43 -0700

    add payload validation to list reducer

--- src/reducer/list.js
+++ src/reducer/list.js
@@ -1,13 +1,21 @@
+let validateList = (payload) => {
+  if(!payload.title)
+    return new Error('VALIDATION_ERROR: list must have title');
+};
+
 export default (state=[], action) => {
   let {type, payload} = action;
 
   switch(type) {
     case 'LIST_CREATE':
+      validateList(payload);
       return [...state, payload];
     case 'LIST_UPDATE':
+      validateList(payload);
       return state.map( (item) =>
         item.id == payload.id ? payload : item );
     case 'LIST_DELETE':
+      validateList(payload);
       return state.filter( (item) =>
         item.id !== payload.id );
     default:
